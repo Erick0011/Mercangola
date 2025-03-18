ab590e78b584699837666bf7dddd3a7edd0cda30
eric <erick.baptista00@gmail.com>
2025-03-19 00:17:48 +0100

    Configuracao inicial

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from app.extensions import db
+from app.config import Config
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Inicializa banco de dados
+    db.init_app(app)
+
+    # Registra rotas
+    from app.routes import public, dashboard, store, admin
+    app.register_blueprint(public.bp)
+    app.register_blueprint(dashboard.bp, url_prefix="/dashboard")
+    app.register_blueprint(store.bp, url_prefix="/store")
+    app.register_blueprint(admin.bp, url_prefix="/admin")
+
+    return app
