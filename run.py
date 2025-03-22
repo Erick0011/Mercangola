9b58ec15546a86d21f0fd703f3cee3ac72815259
eric <erick.baptista00@gmail.com>
2025-03-22 11:14:55 +0100

    Criei tre users teste

--- run.py
+++ run.py
@@ -1,10 +1,42 @@
 from app import create_app
 from app.extensions import db
+from app.models.user import User, UserRole
 
 app = create_app()
 
 with app.app_context():
-    db.create_all()
+    db.create_all()  # Cria todas as tabelas
+
+    # Verificar se já existem usuários para evitar duplicação
+    if not User.query.first():
+        users = [
+            User(
+                email="admin@example.com",
+                name="Admin User",
+                telefone="999999999",
+                role=UserRole.ADMIN,
+            ),
+            User(
+                email="store@example.com",
+                name="Store Owner",
+                telefone="888888888",
+                role=UserRole.STORE_OWNER,
+            ),
+            User(
+                email="client@example.com",
+                name="Client User",
+                telefone="777777777",
+                role=UserRole.CLIENT,
+            ),
+        ]
+
+        # Definir senha para cada usuário
+        for user in users:
+            user.set_password("12345")  # Senha será armazenada com hash
+            db.session.add(user)
+
+        db.session.commit()
+        print("Banco de dados criado e usuários inseridos com sucesso!")
 
 if __name__ == "__main__":
     app.run(debug=True)
