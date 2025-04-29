5bef770ee1c92075652cfd661a2a4b22d28e1f61
eric <erick.baptista00@gmail.com>
2025-04-29 15:25:00 +0100

    aaa

--- app/services/form_StoreOwnerRegistration.py
+++ app/services/form_StoreOwnerRegistration.py
@@ -1,39 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField, TextAreaField, FloatField
-from wtforms.validators import DataRequired, Email, Length, EqualTo, Optional, URL
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
 
 
 class StoreOwnerRegistrationForm(FlaskForm):
-    # Campos do usuário (User)
+    # Usuário
     name = StringField("Nome", validators=[DataRequired(), Length(min=2, max=100)])
     email = StringField("Email", validators=[DataRequired(), Email()])
-    telefone = StringField("Telefone", validators=[Optional(), Length(min=9, max=20)])
     password = PasswordField("Senha", validators=[DataRequired(), Length(min=6)])
     confirm_password = PasswordField("Confirmar Senha", validators=[DataRequired(), EqualTo("password")])
 
-    # Campos da loja (Store)
+    # Loja
     store_name = StringField("Nome da Loja", validators=[DataRequired(), Length(min=2, max=255)])
-    slug = StringField("Slug (URL única da loja)", validators=[DataRequired(), Length(min=2, max=100)])
-    description = TextAreaField("Descrição da Loja", validators=[Optional(), Length(max=500)])
+    slug = StringField("Slug da Loja (URL única)", validators=[DataRequired(), Length(min=2, max=100)])
 
-    # Contato da loja
-    phone = StringField("Telefone da Loja", validators=[Optional(), Length(min=9, max=20)])
-    email_store = StringField("Email da Loja", validators=[Optional(), Email()])
-    website = StringField("Website", validators=[Optional(), URL()])
-
-    # Endereço da loja
-    address = StringField("Endereço", validators=[Optional(), Length(max=255)])
-    city = StringField("Cidade", validators=[Optional(), Length(max=100)])
-    state = StringField("Estado", validators=[Optional(), Length(max=100)])
-    country = StringField("País", validators=[Optional(), Length(max=100)])
-    latitude = FloatField("Latitude", validators=[Optional()])
-    longitude = FloatField("Longitude", validators=[Optional()])
-
-    # Redes sociais
-    facebook = StringField("Facebook", validators=[Optional(), URL()])
-    instagram = StringField("Instagram", validators=[Optional(), URL()])
-    tiktok = StringField("Tiktok", validators=[Optional(), URL()])
-
-
-
-    submit = SubmitField("Registrar Loja")
+    submit = SubmitField("Criar Loja")
